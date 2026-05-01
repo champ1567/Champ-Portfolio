bcf4e3208ba73abcbca12c79f1332d5b97959746
champ15567 <145868761+champ15567@users.noreply.github.com>
2026-05-01 07:45:18 +0700

    install prettier and update eslint
    ติดตั้ง prettier  แล้วให้ prettier ใช้งานร่วมกับ eslint ได้อย่างถูกต้อง

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,10 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import prettierConfig from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -12,10 +14,17 @@ export default defineConfig([
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      prettierConfig,
     ],
     languageOptions: {
       globals: globals.browser,
       parserOptions: { ecmaFeatures: { jsx: true } },
     },
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'warn',
+    },
   },
-])
+]);
--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const a = 'dasda';
+  console.log(a);
 
   return (
-    <><div>
-       <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  </div>
+    <>
+      <div>
+        <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
